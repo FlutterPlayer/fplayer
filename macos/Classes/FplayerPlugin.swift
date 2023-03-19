790958a20a81a8fbe2099d52de9ee289507537b2
biless <biless@qq.com>
2023-03-19 16:33:28 +0800

    feat: 添加MacOS的框架

--- macos/Classes/FplayerPlugin.swift
+++ macos/Classes/FplayerPlugin.swift
@@ -21,4 +21,19 @@ public class FplayerPlugin: NSObject, FlutterPlugin {
             result(FlutterMethodNotImplemented)
         }
     }
+
+  public static func register(with registrar: FlutterPluginRegistrar) {
+    let channel = FlutterMethodChannel(name: "fplayer", binaryMessenger: registrar.messenger)
+    let instance = FplayerPlugin()
+    registrar.addMethodCallDelegate(instance, channel: channel)
+  }
+
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    switch call.method {
+    case "getPlatformVersion":
+      result("macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
+    default:
+      result(FlutterMethodNotImplemented)
+    }
+  }
 }
