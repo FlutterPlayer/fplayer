bb5b77b7ff6c31215bbae467a5d50134ce79e4e4
ZWFON\zwf97 <953615108@qq.com>
2023-03-24 11:35:37 +0800

    feat: 退出播放器后恢复设备亮度

--- example/lib/video_page.dart
+++ example/lib/video_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:fplayer/fplayer.dart';
+import 'package:screen_brightness/screen_brightness.dart';
 
 import 'app_bar.dart';
 
@@ -252,8 +253,14 @@ class VideoScreenState extends State<VideoScreen> {
   }
 
   @override
-  void dispose() {
+  void dispose() async {
     super.dispose();
+    try {
+      await ScreenBrightness().resetScreenBrightness();
+    } catch (e) {
+      print(e);
+      throw 'Failed to reset brightness';
+    }
     player.release();
   }
 }
