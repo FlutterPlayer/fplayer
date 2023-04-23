19bccdd37ec7b0bb21a10ed68a59f3bf9ff6dbca
biless <biless@qq.com>
2023-04-23 20:45:14 +0800

    feat: 添加mac objective-c支持

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,12 @@ import Foundation
 
 import battery_plus
 import fplayer
+import screen_brightness_macos
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BatteryPlusMacosPlugin.register(with: registry.registrar(forPlugin: "BatteryPlusMacosPlugin"))
   FplayerPlugin.register(with: registry.registrar(forPlugin: "FplayerPlugin"))
+  ScreenBrightnessMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenBrightnessMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
---
+++ macos/Classes/Fplayer.h
@@ -0,0 +1,15 @@
+//
+//  Fplayer.h
+//  Pods
+//
+//  Created by Biles on 2023/4/23.
+//
+
+#ifndef Fplayer_h
+#define Fplayer_h
+
+
+void add(int a,int b);
+
+
+#endif /* Fplayer_h */
