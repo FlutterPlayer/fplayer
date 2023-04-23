535af585ce7b712021efe5f01e8476e84b32297d
biless <biless@qq.com>
2023-04-23 20:45:14 +0800

    feat: 添加mac objective-c支持

--- macos/Classes/FplayerPlugin.swift
+++ macos/Classes/FplayerPlugin.swift
@@ -2,18 +2,23 @@ import Cocoa
 import FlutterMacOS
 
 public class FplayerPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "fplayer", binaryMessenger: registrar.messenger)
-    let instance = FplayerPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
-    default:
-      result(FlutterMethodNotImplemented)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "fplayer", binaryMessenger: registrar.messenger)
+        let instance = FplayerPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "getPlatformVersion":
+            result("macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
+            
+        case "":
+            add(1, 2)
+            
+            result(FlutterMethodNotImplemented)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
 }
